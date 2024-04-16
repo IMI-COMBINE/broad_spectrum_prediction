850a8864525198d8a0c713a27bf5ebeaeb47fdbc
Yojana Gadiya <45199062+YojanaGadiya@users.noreply.github.com>
2024-04-16 09:37:50 +0200

    update data processing scripts

---
+++ preprocess/utils.py
@@ -0,0 +1,677 @@
+"""Python script with util functions."""
+
+from statistics import median
+import pandas as pd
+
+from itertools import combinations
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+from matplotlib.patches import Ellipse
+
+DATA_DIR = "../data"
+
+
+# get shared elements for each combination of sets
+def get_shared(sets):
+    IDs = sets.keys()
+    combs = sum(
+        [list(map(list, combinations(IDs, i))) for i in range(1, len(IDs) + 1)], []
+    )
+
+    shared = {}
+    for comb in combs:
+        ID = " and ".join(comb)
+        if len(comb) == 1:
+            shared.update({ID: sets[comb[0]]})
+        else:
+            setlist = [sets[c] for c in comb]
+            u = set.intersection(*setlist)
+            shared.update({ID: u})
+    return shared
+
+
+# get unique elements for each combination of sets
+def get_unique(shared):
+    unique = {}
+    for shar in shared:
+        if shar == list(shared.keys())[-1]:
+            s = shared[shar]
+            unique.update({shar: s})
+            continue
+        count = shar.count(" and ")
+        if count == 0:
+            setlist = [
+                shared[k] for k in shared.keys() if k != shar and " and " not in k
+            ]
+            s = shared[shar].difference(*setlist)
+        else:
+            setlist = [
+                shared[k]
+                for k in shared.keys()
+                if k != shar and k.count(" and ") >= count
+            ]
+            s = shared[shar].difference(*setlist)
+        unique.update({shar: s})
+    return unique
+
+
+# plot Venn
+def draw_venn(sets={}, size=3.5):
+    shared = get_shared(sets)
+    unique = get_unique(shared)
+    ce = "bgrc"  # colors
+    lw = size * 0.12  # line width
+    fs = size * 2  # font size
+    nc = 2  # legend cols
+    cs = 4  # columnspacing
+
+    ax = plt.figure(figsize=(size, size), dpi=200).add_subplot(111)
+
+    ax.set_xlim(0, 100)
+    ax.set_ylim(0, 100)
+    ax.axis("off")
+
+    # 4 sets
+    if len(sets) == 4:
+        # draw ellipses
+        ew = 45  # width
+        eh = 75  # height
+        xe = [35, 48, 52, 65]  # x coordinats
+        ye = [35, 45, 45, 35]  # y coordinats
+        ae = [225, 225, 315, 315]  # angles
+
+        for i, s in enumerate(sets):
+            ax.add_artist(
+                Ellipse(
+                    xy=(xe[i], ye[i]),
+                    width=ew,
+                    height=eh,
+                    fc=ce[i],
+                    angle=ae[i],
+                    alpha=0.3,
+                )
+            )
+            ax.add_artist(
+                Ellipse(
+                    xy=(xe[i], ye[i]),
+                    width=ew,
+                    height=eh,
+                    fc="None",
+                    angle=ae[i],
+                    ec="black",
+                    lw=lw,
+                )
+            )
+
+        # annotate
+        xt = [
+            12,
+            32,
+            68,
+            88,
+            14,
+            34,
+            66,
+            86,
+            26,
+            28,
+            50,
+            50,
+            72,
+            74,
+            37,
+            60,
+            40,
+            63,
+            50,
+        ]  # x
+        yt = [
+            67,
+            79,
+            79,
+            67,
+            41,
+            70,
+            70,
+            41,
+            59,
+            26,
+            11,
+            60,
+            26,
+            59,
+            51,
+            17,
+            17,
+            51,
+            35,
+        ]  # y
+
+        for j, s in enumerate(sets):
+            ax.text(
+                xt[j],
+                yt[j],
+                "",
+                ha="center",
+                va="center",
+                fontsize=fs,
+                transform=ax.transData,
+            )
+
+        for k in unique:
+            j += 1
+            ax.text(
+                xt[j],
+                yt[j],
+                len(unique[k]),
+                ha="center",
+                va="center",
+                fontsize=fs,
+                transform=ax.transData,
+            )
+
+    # 3 sets
+    if len(sets) == 3:
+        # draw circles
+        ew = 60  # width
+        eh = 60  # height
+        lw = size * 0.12  # line width
+        xe = [37, 63, 50]  # x coordinats
+        ye = [55, 55, 32]  # y coordinats
+        nc = 3  # legend columns
+        cs = 1  # columns spacing
+
+        for i, s in enumerate(sets):
+            ax.add_artist(
+                Ellipse(
+                    xy=(xe[i], ye[i]), width=ew, height=eh, fc=ce[i], angle=0, alpha=0.3
+                )
+            )
+            ax.add_artist(
+                Ellipse(
+                    xy=(xe[i], ye[i]),
+                    width=ew,
+                    height=eh,
+                    fc="None",
+                    angle=0,
+                    ec="black",
+                    lw=lw,
+                )
+            )
+
+        # annotate
+        xt = [12, 88, 28, 22, 78, 50, 50, 30, 70, 50]  # x
+        yt = [80, 80, 3, 60, 60, 17, 70, 35, 35, 50]  # y
+
+        for j, s in enumerate(sets):
+            ax.text(
+                xt[j],
+                yt[j],
+                "",
+                ha="center",
+                va="center",
+                fontsize=fs,
+                transform=ax.transData,
+            )
+
+        for k in unique:
+            j += 1
+            ax.text(
+                xt[j],
+                yt[j],
+                len(unique[k]),
+                ha="center",
+                va="center",
+                fontsize=fs,
+                transform=ax.transData,
+            )
+
+    # 2 sets
+    if len(sets) == 2:
+        # draw circles
+        ew = 70  # width
+        eh = 70  # height
+        lw = size * 0.12  # line width
+        xe = [37, 63]  # x coordinats
+        ye = [45, 45]  # y coordinats
+
+        for i, s in enumerate(sets):
+            ax.add_artist(
+                Ellipse(
+                    xy=(xe[i], ye[i]), width=ew, height=eh, fc=ce[i], angle=0, alpha=0.3
+                )
+            )
+            ax.add_artist(
+                Ellipse(
+                    xy=(xe[i], ye[i]),
+                    width=ew,
+                    height=eh,
+                    fc="None",
+                    angle=0,
+                    ec="black",
+                    lw=lw,
+                )
+            )
+
+        # annotate
+        xt = [20, 80, 18, 82, 50]  # x
+        yt = [80, 80, 45, 45, 45]  # y
+
+        for j, s in enumerate(sets):
+            ax.text(
+                xt[j],
+                yt[j],
+                "",
+                ha="center",
+                va="center",
+                fontsize=fs,
+                transform=ax.transData,
+            )
+
+        for k in unique:
+            j += 1
+            ax.text(
+                xt[j],
+                yt[j],
+                len(unique[k]),
+                ha="center",
+                va="center",
+                fontsize=fs,
+                transform=ax.transData,
+            )
+
+    # legend
+    handles = [
+        mpatches.Patch(color=ce[i], label=l, alpha=0.3) for i, l in enumerate(sets)
+    ]
+    ax.legend(
+        labels=sets,
+        handles=handles,
+        fontsize=fs * 1.1,
+        frameon=False,
+        bbox_to_anchor=(0.5, 0.99),
+        bbox_transform=ax.transAxes,
+        loc=9,
+        handlelength=1.5,
+        ncol=nc,
+        columnspacing=cs,
+        handletextpad=0.5,
+    )
+
+
+"""Processing SPARK Files"""
+
+
+def _process_spark_physchem(df: pd.DataFrame):
+    cols_of_interest = [
+        "Compound Name",
+        "SMILES",
+        "O'Shea 2008 J. Med. Chem. 51:10 - Summary Data of 147 Antibacterials: Gram_pos",
+        "O'Shea 2008 J. Med. Chem. 51:10 - Summary Data of 147 Antibacterials: Gram_neg",
+        "O'Shea 2008 J. Med. Chem. 51:10 - Summary Data of 147 Antibacterials: P_aeruginosa",
+    ]
+
+    df = df.astype(
+        {
+            "O'Shea 2008 J. Med. Chem. 51:10 - Summary Data of 147 Antibacterials: Gram_pos": float,
+            "O'Shea 2008 J. Med. Chem. 51:10 - Summary Data of 147 Antibacterials: Gram_neg": float,
+            "O'Shea 2008 J. Med. Chem. 51:10 - Summary Data of 147 Antibacterials: P_aeruginosa": float,
+        }
+    )
+
+    df = df[cols_of_interest]
+
+    data = []
+
+    for row in df.values:
+        compound_name, smiles, gram_pos, gram_neg, p_aeruginosa = row
+
+        if pd.isna(smiles):
+            continue
+
+        if gram_pos > 0:
+            data.append(
+                {
+                    "compound_name": compound_name,
+                    "smiles": smiles,
+                    "standard_relation": "=",
+                    "standard_value": gram_pos,
+                    "standard_units": "ug.mL-1",
+                    "bact_class": "gram-positive",
+                }
+            )
+
+        if gram_neg > 0 and p_aeruginosa > 0:
+            data.append(
+                {
+                    "compound_name": compound_name,
+                    "smiles": smiles,
+                    "standard_relation": "=",
+                    "standard_value": median([gram_neg, p_aeruginosa]),
+                    "standard_units": "ug.mL-1",
+                    "bact_class": "gram-negative",
+                }
+            )
+
+    final_df = pd.DataFrame(data)
+    possible_values = {"=", ">", "<"}
+    found_values = set(final_df["standard_relation"].unique())
+    assert len(found_values.difference(possible_values)) == 0, found_values
+    return final_df
+
+
+def _process_spark_achogen(df: pd.DataFrame):
+    cols_of_interest = [
+        "Compound Name",
+        "SMILES",
+        "Curated & Transformed MIC Data: MIC (in µM) (µM)",
+        "Curated & Transformed MIC Data: Species",
+        "Achaogen LpxC MIC Data: MIC (in uM) (uM)",
+        "Achaogen LpxC MIC Data: Species",
+    ]
+
+    df = df[cols_of_interest]
+
+    data = []
+
+    for row in df.values:
+        compound_name, smiles, mic, species, lpxc_mic, lpxc_species = row
+
+        if pd.isna(smiles):
+            continue
+
+        if pd.notna(mic) and pd.notna(species):
+            rel = mic.split(" ")[0]
+
+            data.append(
+                {
+                    "compound_name": compound_name,
+                    "smiles": smiles,
+                    "standard_relation": "=" if rel not in ["<", ">"] else rel,
+                    "standard_value": mic,
+                    "standard_units": "ug.mL-1",
+                    "organism": species,
+                }
+            )
+
+        if pd.notna(lpxc_mic) and pd.notna(lpxc_species):
+            lpxc_rel = lpxc_mic.split(" ")[0]
+
+            data.append(
+                {
+                    "compound_name": compound_name,
+                    "smiles": smiles,
+                    "standard_relation": (
+                        "=" if lpxc_rel not in ["<", ">"] else lpxc_rel
+                    ),
+                    "standard_value": lpxc_mic,
+                    "standard_units": "ug.mL-1",
+                    "organism": lpxc_species,
+                }
+            )
+
+    final_df = pd.DataFrame(data)
+    possible_values = {"=", ">", "<"}
+    found_values = set(final_df["standard_relation"].unique())
+    assert len(found_values.difference(possible_values)) == 0, found_values
+    return final_df
+
+
+def _process_spark_mic(df: pd.DataFrame):
+    cols_of_interest = [
+        "Compound Name",
+        "SMILES",
+        "Curated & Transformed MIC Data: MIC (µg/mL)",
+        "Curated & Transformed MIC Data: Species",
+        "Extracted & Uploaded MIC Data: MIC (µg/mL)",
+        "Extracted & Uploaded MIC Data: Species",
+    ]
+
+    df = df[cols_of_interest]
+
+    data = []
+
+    for row in df.values:
+        compound_name, smiles, mic, species, extracted_mic, extracted_species = row
+
+        if pd.isna(smiles):
+            continue
+
+        if pd.notna(mic) and pd.notna(species):
+            rel = mic.split(" ")[0]
+            data.append(
+                {
+                    "compound_name": compound_name,
+                    "smiles": smiles,
+                    "standard_relation": "=" if rel not in ["<", ">"] else rel,
+                    "standard_value": mic,
+                    "standard_units": "ug.mL-1",
+                    "organism": species,
+                }
+            )
+
+        if pd.notna(extracted_mic) and pd.notna(extracted_species):
+            extracted_rel = extracted_mic.split(" ")[0]
+            data.append(
+                {
+                    "compound_name": compound_name,
+                    "smiles": smiles,
+                    "standard_relation": (
+                        "=" if extracted_rel not in ["<", ">"] else extracted_rel
+                    ),
+                    "standard_value": extracted_mic,
+                    "standard_units": "ug.mL-1",
+                    "organism": extracted_species,
+                }
+            )
+
+    final_df = pd.DataFrame(data)
+    possible_values = {"=", ">", "<"}
+    found_values = set(final_df["standard_relation"].unique())
+    assert len(found_values.difference(possible_values)) == 0, found_values
+    return final_df
+
+
+def _process_spark_novartis(df: pd.DataFrame):
+    cols_of_interest = [
+        "Compound Name",
+        "SMILES",
+        "Curated & Transformed MIC Data: MIC (in µM) (µM)",
+        "Curated & Transformed MIC Data: Species",
+    ]
+
+    df = df[cols_of_interest]
+
+    data = []
+
+    for row in df.values:
+        compound_name, smiles, mic, species = row
+
+        if pd.isna(smiles):
+            continue
+
+        if pd.notna(mic) and pd.notna(species):
+            rel = mic.split(" ")[0]
+
+            data.append(
+                {
+                    "compound_name": compound_name,
+                    "smiles": smiles,
+                    "standard_relation": "=" if rel not in ["<", ">"] else rel,
+                    "standard_value": mic,
+                    "standard_units": "ug.mL-1",
+                    "organism": species,
+                }
+            )
+
+    final_df = pd.DataFrame(data)
+    possible_values = {"=", ">", "<"}
+    found_values = set(final_df["standard_relation"].unique())
+    assert len(found_values.difference(possible_values)) == 0, found_values
+    return final_df
+
+
+def _process_spark_merk(df: pd.DataFrame):
+    cols_of_interest = [
+        "Compound Name",
+        "SMILES",
+        "Curated & Transformed MIC Data: MIC (µg/mL)",
+        "Curated & Transformed MIC Data: Species",
+    ]
+
+    df = df[cols_of_interest]
+
+    data = []
+
+    for row in df.values:
+        compound_name, smiles, mic, species = row
+
+        if pd.isna(smiles):
+            continue
+
+        if pd.notna(mic) and pd.notna(species):
+            rel = mic.split(" ")[0]
+
+            data.append(
+                {
+                    "compound_name": compound_name,
+                    "smiles": smiles,
+                    "standard_relation": "=" if rel not in ["<", ">"] else rel,
+                    "standard_value": mic,
+                    "standard_units": "ug.mL-1",
+                    "organism": species,
+                }
+            )
+
+    final_df = pd.DataFrame(data)
+    possible_values = {"=", ">", "<"}
+    found_values = set(final_df["standard_relation"].unique())
+    assert len(found_values.difference(possible_values)) == 0, found_values
+    return final_df
+
+
+def _process_spark_coadd(df: pd.DataFrame):
+    cols_of_interest = [
+        "Compound Name",
+        "SMILES",
+        "Curated & Transformed MIC Data: MIC (µg/mL)",
+        "Curated & Transformed MIC Data: Species",
+    ]
+
+    df = df[cols_of_interest]
+
+    data = []
+
+    for row in df.values:
+        compound_name, smiles, mic, species = row
+
+        if pd.isna(smiles):
+            continue
+
+        if pd.notna(mic) and pd.notna(species):
+            rel = mic.split(" ")[0]
+
+            data.append(
+                {
+                    "compound_name": compound_name,
+                    "smiles": smiles,
+                    "standard_relation": "=" if rel not in ["<", ">"] else rel,
+                    "standard_value": mic,
+                    "standard_units": "ug.mL-1",
+                    "organism": species,
+                }
+            )
+
+    final_df = pd.DataFrame(data)
+    possible_values = {"=", ">", "<"}
+    found_values = set(final_df["standard_relation"].unique())
+    assert len(found_values.difference(possible_values)) == 0, found_values
+    return final_df
+
+
+def _process_spark_pub(df: pd.DataFrame):
+    cols_of_interest = [
+        "Compound Name",
+        "SMILES",
+        "Porras 2020 Chem. Rev. XX:XXXX: MIC value (µg/mL)",
+        "Porras 2020 Chem. Rev. XX:XXXX: Species",
+        "Chassagne 2020 Front. Pharmacol. 11:586548: MIC value (µg/mL)",
+        "Chassagne 2020 Front. Pharmacol. 11:586548: Species",
+    ]
+
+    df = df[cols_of_interest]
+
+    data = []
+
+    for row in df.values:
+        (
+            compound_name,
+            smiles,
+            porras_mic,
+            porras_species,
+            chassagne_mic,
+            chassagne_species,
+        ) = row
+
+        if pd.isna(smiles):
+            continue
+
+        if pd.notna(porras_mic) and pd.notna(porras_species):
+            rel = porras_mic.split(" ")[0]
+
+            data.append(
+                {
+                    "compound_name": compound_name,
+                    "smiles": smiles,
+                    "standard_relation": "=" if rel not in ["<", ">"] else rel,
+                    "standard_value": porras_mic,
+                    "standard_units": "ug.mL-1",
+                    "organism": porras_species,
+                }
+            )
+
+        if pd.notna(chassagne_mic) and pd.notna(chassagne_species):
+            rel = chassagne_mic.split(" ")[0]
+
+            data.append(
+                {
+                    "compound_name": compound_name,
+                    "smiles": smiles,
+                    "standard_relation": "=" if rel not in ["<", ">"] else rel,
+                    "standard_value": chassagne_mic,
+                    "standard_units": "ug.mL-1",
+                    "organism": chassagne_species,
+                }
+            )
+
+    final_df = pd.DataFrame(data)
+    possible_values = {"=", ">", "<"}
+    found_values = set(final_df["standard_relation"].unique())
+    assert len(found_values.difference(possible_values)) == 0, found_values
+    return final_df
+
+
+def process_spark_data(file: str):
+    """Processing and cleaning SPARK files."""
+    df = pd.read_csv(f"{DATA_DIR}/raw/{file}", dtype=str)
+
+    if "SPARK Data Compounds & Physicochemical Properties.csv" == file:
+        return _process_spark_physchem(df)
+
+    elif "SPARK Data Achaogen Contribution.csv" == file:
+        return _process_spark_achogen(df)
+
+    elif "SPARK MIC Data.csv" == file:
+        return _process_spark_mic(df)
+
+    elif "SPARK Data Novartis Contribution.csv" == file:
+        return _process_spark_novartis(df)
+
+    elif "SPARK Data Merck & Kyorin Contribution.csv" == file:
+        return _process_spark_merk(df)
+
+    elif "SPARK Data CO-ADD Contribution.csv" == file:
+        return _process_spark_coadd(df)
+
+    elif "SPARK Data Quave Lab {Emory University} Publications.csv" == file:
+        return _process_spark_pub(df)
